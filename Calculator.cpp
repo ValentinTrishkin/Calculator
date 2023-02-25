d2d239decc75f4791c6793049518385ad9cecb64
valentine86 <ya@vtrishkin.ru>
2023-02-25 17:10:30 +0300

    Change Calculator.cpp

--- Calculator.cpp
+++ Calculator.cpp
@@ -5,54 +5,57 @@
 
 class Calculator
 {
-public:
-    double num1;
-    double num2;
+public:    
     double add()
     {
-        return num1 + num2;
+        return num1_ + num2_;
     }
     double multiply()
     {
-        return num1 * num2;
+        return num1_ * num2_;
     }
     double subtract_1_2()
     {
-        return num2 - num1;
+        return num2_ - num1_;
     }
     double subtract_2_1()
     {
-        return num1 - num2;
+        return num1_ - num2_;
     }
     double divide_1_2()
     {
-        return num2 / num1;
+        return num2_ / num1_;
     }
     double divide_2_1()
     {
-        return num1 / num2;
+        return num1_ / num2_;
     }
     bool set_num1(double num1)
     {
-        if (this->num1 != 0)
+        if (num1 != 0)
         {
+            num1_ = num1;
             return true;
         }
         return false;
     }
     bool set_num2(double num2)
     {
-        if (this->num2 != 0)
+        if (num2 != 0)
         {
+            num2_ = num2;
             return true;
         }
         return false;
     }
     Calculator(double num1, double num2)
     {
-        this->num1 = num1;
-        this->num2 = num2;
+        num1_ = num1;
+        num2_ = num2;
     }
+private:
+    double num1_;
+    double num2_;
 };
 
 
@@ -68,52 +71,24 @@ int main()
         Calculator test(num1, num2);
         std::cout << "Введите num1: ";
         std::cin >> num1;
-        std::cout << "Введите num2: ";
-        std::cin >> num2;
-        test.num1 = num1;
-        test.num2 = num2;
-       /* do
+        if (test.set_num1(num1))
         {
-
-        } while (true);*/
-        if (test.set_num1(num1)&&test.set_num2(num2))
-        {
-            std::cout << "num1 + num2 = " << test.add() << std::endl;
-            std::cout << "num1 - num2 = " << test.subtract_2_1() << std::endl;
-            std::cout << "num2 - num1 = " << test.subtract_1_2() << std::endl;
-            std::cout << "num1 * num2 = " << test.multiply() << std::endl;
-            std::cout << "num1 / num2 = " << test.divide_2_1() << std::endl;
-            std::cout << "num2 / num1 = " << test.divide_1_2() << std::endl;
-        }
-        else
-        {
-            std::cout << "Неверный ввод!" << std::endl;
-            if (!test.set_num1(num1))
-            {
-                std::cout << "Введите num1: ";
-                std::cin >> num1;
-                test.num1 = num1;
-                std::cout << "num1 + num2 = " << test.add() << std::endl;
-                std::cout << "num1 - num2 = " << test.subtract_2_1() << std::endl;
-                std::cout << "num2 - num1 = " << test.subtract_1_2() << std::endl;
-                std::cout << "num1 * num2 = " << test.multiply() << std::endl;
-                std::cout << "num1 / num2 = " << test.divide_2_1() << std::endl;
-                std::cout << "num2 / num1 = " << test.divide_1_2() << std::endl;
-            }
-            if (!test.set_num2(num2))
+            do
             {
                 std::cout << "Введите num2: ";
                 std::cin >> num2;
-                test.num2 = num2;
-                std::cout << "num1 + num2 = " << test.add() << std::endl;
-                std::cout << "num1 - num2 = " << test.subtract_2_1() << std::endl;
-                std::cout << "num2 - num1 = " << test.subtract_1_2() << std::endl;
-                std::cout << "num1 * num2 = " << test.multiply() << std::endl;
-                std::cout << "num1 / num2 = " << test.divide_2_1() << std::endl;
-                std::cout << "num2 / num1 = " << test.divide_1_2() << std::endl;
-               
-            }
-        }
+                if (test.set_num2(num2))
+                {
+                    std::cout << "num1 + num2 = " << test.add() << std::endl;
+                    std::cout << "num1 - num2 = " << test.subtract_2_1() << std::endl;
+                    std::cout << "num2 - num1 = " << test.subtract_1_2() << std::endl;
+                    std::cout << "num1 * num2 = " << test.multiply() << std::endl;
+                    std::cout << "num1 / num2 = " << test.divide_2_1() << std::endl;
+                    std::cout << "num2 / num1 = " << test.divide_1_2() << std::endl;
+                }
+                else (std::cout << "Неверный ввод!" << std::endl);
+            } while (!test.set_num2(num2));        
+        } else (std::cout << "Неверный ввод!" << std::endl);
     } while (true);
 
    
